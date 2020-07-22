2e8bcafca58cb61c60cbccd6af2d5011e683748b
Nazmul <grey000906@gmail.com>
2020-07-23 05:24:07 +0600

    nav & sidebar

---
+++ src/App.vue
@@ -0,0 +1,30 @@
+<template>
+  <div id="app">
+    <div class="container-scroller">
+      <app-header />
+      <div class="container-fluid page-body-wrapper">
+        <app-sidebar />
+        <div class="main-panel">
+          <div class="content-wrapper">
+            <router-view></router-view>
+          </div>
+
+          <app-footer />
+        </div>
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+import AppHeader from "../src/components/partials/AppHeader";
+import AppSidebar from "../src/components/partials/AppSidebar";
+import AppFooter from "../src/components/partials/AppFooter";
+export default {
+  name: "app",
+  components: {
+    AppHeader,
+    AppSidebar,
+    AppFooter,
+  },
+};
+</script>
---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue";
+import App from "./App.vue";
+import "./registerServiceWorker";
+import router from "./router";
+
+import "@/assets/scss/style.scss";
+import BootstrapVue from "bootstrap-vue";
+
+Vue.use(BootstrapVue);
+
+Vue.config.productionTip = false;
+
+new Vue({
+  router,
+  render: (h) => h(App),
+}).$mount("#app");
